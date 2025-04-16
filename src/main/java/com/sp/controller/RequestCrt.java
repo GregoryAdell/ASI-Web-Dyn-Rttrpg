9b883aff2ad22a4df5e5c9ec52c78367e945b044
lbonnieul <lucas.bonnieul1@gmail.com>
2025-04-16 13:51:12 +0200

    Step1 finished

--- src/main/java/com/sp/controller/RequestCrt.java
+++ src/main/java/com/sp/controller/RequestCrt.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.sp.model.Poney;
+import com.sp.model.PoneyFormDTO;
+
 @Controller // AND NOT @RestController
 public class RequestCrt {
 
@@ -28,9 +31,29 @@ public class RequestCrt {
     }
     
     @RequestMapping(value = { "/view"}, method = RequestMethod.GET)
-        public String view(Model model) {
+    public String view(Model model) {
         model.addAttribute("myPoney",poneyDao.getRandomPoney() );
         return "poneyView";
+    }
+
+    @RequestMapping(value = { "/addPoney"}, method = RequestMethod.GET)
+    public String addponey(Model model) {
+        PoneyFormDTO poneyForm = new PoneyFormDTO();
+        model.addAttribute("poneyForm", poneyForm);
+        return "poneyForm";
+    }
+
+    @RequestMapping(value = { "/addPoney"}, method = RequestMethod.POST)
+    public String addponey(Model model, @ModelAttribute("poneyForm") PoneyFormDTO poneyForm) {
+        Poney p=poneyDao.addPoney(poneyForm.getName(),poneyForm.getColor(),poneyForm.getSuperPower(),poneyForm.getImgUrl());
+        model.addAttribute("myPoney",p );
+        return "poneyView";
+    }
+
+    @RequestMapping(value = { "/list"}, method = RequestMethod.GET)
+    public String viewList(Model model) {
+        model.addAttribute("poneyList",poneyDao.getPoneyList() );
+        return "poneyViewList";
     }
 
 }
--- src/main/java/com/sp/model/PoneyFormDTO.java
+++ src/main/java/com/sp/model/PoneyFormDTO.java
@@ -1,23 +1,30 @@
 package com.sp.model;
 
-public class Poney  {
+public class PoneyFormDTO {
 	private String color;
 	private String superPower;
 	private String name;
 	private String imgUrl;
 
-	public Poney() {
+	public String getImgUrl() {
+		return imgUrl;
+	}
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+	public PoneyFormDTO() {
 		this.color = "";
 		this.superPower = "";
 		this.name = "";
 		this.imgUrl="";
 	}
-	public Poney(String name,String color,String superPower, String imgUrl) {
+	public PoneyFormDTO(String name,String color,String superPower, String imgUrl) {
 		this.color = color;
 		this.superPower = superPower;
 		this.name = name;
 		this.imgUrl=imgUrl;
 	}
+
     public String getColor() {
         return color;
     }
@@ -36,12 +43,5 @@ public class Poney  {
     public void setName(String name) {
         this.name = name;
     }
-    public String getImgUrl() {
-        return imgUrl;
-    }
-    public void setImgUrl(String imgUrl) {
-        this.imgUrl = imgUrl;
-    }
-
-  
+    
 }
